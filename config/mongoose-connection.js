a05116976f0342ed77abbead4e72d49e3701142f
Sachin Bajaj <bajajsachin101@gmail.com>
2025-03-23 13:18:51 +0530

    mongo

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 const config = require("config");
 
+const MONGO_URI = process.env.MONGO_URI || config.get("MONGODB_URI");
+
 mongoose
-.connect(`${config.get("MONGODB_URI")}/debateapp`)
-.then(function(){
-    dbgr("Connected")
-})
-.catch(function(err){
-    dbgr(err)
-})
+  .connect(`${MONGO_URI}/debateapp`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => dbgr("MongoDB Connected!"))
+  .catch((err) => dbgr("MongoDB Connection Error:", err));
 
 module.exports = mongoose.connection;
