6ac5fe416008b03442e88976f0115d07d0c5cf32
Sachin Bajaj <bajajsachin101@gmail.com>
2025-03-23 15:27:21 +0530

    Front-page

--- router/user.js
+++ router/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../mongoose/user-mongo");
-const bcrypt = require('bcryptjs');
-;
+const bcrypt = require("bcrypt");
 const { sendPushNotificationAll, sendPushNotification } = require("../services/firebase");
 const jwt = require("jsonwebtoken");
 const {userregister, loginuser, logout, verifyOtp, resendOtp} = require("../controller/authcontroller");
