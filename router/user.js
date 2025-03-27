13220f8a9339e8e8266535a70c7727abf526919a
Sachin Bajaj <bajajsachin101@gmail.com>
2025-03-27 18:05:34 +0530

    mn

--- router/user.js
+++ router/user.js
@@ -21,6 +21,7 @@ const mongoose = require("mongoose");
 const { conn, getGFS } = require("../config/gridfs");
 const  SendEmail = require("../config/nodemailer");
 const nodeCache = require("../controller/Cache");
+const Comment = require("../mongoose/comment-mongoose");
 
 router.get("/register", (req, res) => {                                                                      //register page
     let err = req.flash("key")
@@ -120,7 +121,8 @@ router.get("/debate/:id", isloggedin, async function(req, res) {
     if (!vedios) {
       vedios = await videomongoose.findById(req.params.id)
         .select("creator comment Views viewedBy vedio")
-        .lean();
+        .lean()
+        .exec();
 
       if (!vedios) {
         return res.status(404).send("Video not found");
@@ -128,20 +130,22 @@ router.get("/debate/:id", isloggedin, async function(req, res) {
 
       // Fetch creator, comments, and suggested videos in parallel for max speed
       const [creator, comments, suggestions] = await Promise.all([
-        User.findById(vedios.creator).select("username followers Rankpoints").lean(),
-        Comment.find({ _id: { $in: vedios.comment } })
+        User.findById(vedios.creator).select("username followers Rankpoints").lean().exec(),
+        Comment.find({ _id: { $in: vedios.comment || [] } }) // Prevent error if no comments
           .select("text userId")
           .populate("userId", "username profile")
-          .lean(),
+          .lean()
+          .exec(),
         videomongoose.find({ _id: { $ne: vedios._id } })
           .select("vedio creator")
           .populate("creator", "username")
           .limit(5)
           .lean()
+          .exec()
       ]);
 
-      // Assign fetched data
-      vedios.creator = creator;
+      // Assign fetched data (ensure creator exists)
+      vedios.creator = creator || {};
       vedios.comment = comments;
 
       // Store full response in cache
@@ -149,15 +153,15 @@ router.get("/debate/:id", isloggedin, async function(req, res) {
     }
 
     // Get logged-in user
-    let user = await User.findOne({ email: req.user.email }).lean();
+    let user = await User.findOne({ email: req.user.email }).lean().exec();
 
     const followerscount = vedios.creator.followers || [];
     const follower = followerscount.length;
-    const isFollowing = followerscount.includes(req.user._id);
-    
+    const isFollowing = followerscount.map(id => id.toString()).includes(req.user._id.toString());
+
     let updateNeeded = false;
 
-    if (!vedios.viewedBy.includes(req.user._id)) {
+    if (!vedios.viewedBy.map(id => id.toString()).includes(req.user._id.toString())) {
       vedios.viewedBy.push(req.user._id);
       updateNeeded = true;
     }
@@ -167,11 +171,11 @@ router.get("/debate/:id", isloggedin, async function(req, res) {
         videomongoose.updateOne(
           { _id: vedios._id },
           { $inc: { Views: 1 }, $push: { viewedBy: req.user._id } }
-        ),
+        ).exec(),
         User.updateOne(
           { _id: vedios.creator._id },
           { $inc: { Rankpoints: 10 } }
-        )
+        ).exec()
       ]);
 
       await User.updateRanks();
@@ -193,9 +197,10 @@ router.get("/debate/:id", isloggedin, async function(req, res) {
 
   } catch (err) {
     console.error("Error fetching video:", err);
-    res.status(500).send("Server error");
+    res.status(500).send("Serv");
   }
 });
+
 
 
 router.get("/video/stream/:id", async (req, res) => {
